792d0443b3e34e0a99a68e254084079ad9356db5
Matthew Fisher <matt.fisher@microsoft.com>
2018-07-05 08:47:05 -0700

    add checks for input string is a valid URL

--- browser_darwin.go
+++ browser_darwin.go
@@ -3,11 +3,16 @@
 package browser
 
 import (
+	"net/url"
 	"os/exec"
 )
 
 // Open opens a url with the default browser.
 func Open(u string) (err error) {
+	_, err = url.ParseRequestURI(u)
+	if err != nil {
+		return
+	}
 	_, err = exec.Command("open", u).Output()
 	return
 }
--- browser_windows.go
+++ browser_windows.go
@@ -3,11 +3,16 @@
 package browser
 
 import (
+	"net/url"
 	"os/exec"
 )
 
 // Open opens a URL with the default browser.
 func Open(u string) (err error) {
+	_, err = url.ParseRequestURI(u)
+	if err != nil {
+		return
+	}
 	_, err = exec.Command("cmd", "/c", "start", u).Output()
 	return
 }
