6e21a0d007a360a113934bbaa7b6a67c1303b67b
Radu Matei <matei.radu94@gmail.com>
2018-07-24 17:20:46 +0300

    Check WSL

--- browser_linux.go
+++ browser_linux.go
@@ -3,6 +3,9 @@
 package browser
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/url"
 	"os/exec"
 )
@@ -13,6 +16,19 @@ func Open(u string) (err error) {
 	if err != nil {
 		return
 	}
+
+	// read /proc/version which contains OS version info
+	data, err := ioutil.ReadFile("/proc/version")
+	if err != nil {
+		return fmt.Errorf("cannot read /proc/version: %v", err)
+	}
+
+	// version is in format: Linux version 4.4.0-17134-Microsoft
+	if bytes.Contains(data, []byte("-Microsoft")) {
+		_, err = exec.Command("cmd.exe", "/c", "start", u).Output()
+		return
+	}
+
 	_, err = exec.Command("xdg-open", u).Output()
 	return
 }
